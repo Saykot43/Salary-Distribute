c6dafd1c1addc603e3efdf8da59f203b8b385210
Saykot Kumar Barmon <saykot.cse43@gmail.com>
2022-02-17 04:48:22 +0600

    Design updated

--- js/app.js
+++ js/app.js
@@ -17,14 +17,16 @@ document.getElementById('calculation').addEventListener('click', function(){
     if(totalExpenses>totalIncome){
         window.alert('Your income is so low to your expenses');
     }else{
-        document.getElementById('total-expenses').innerText = totalExpenses;
+        document.getElementById('total-input').innerText = totalExpenses;
+
         const totalBalance= totalIncome-totalExpenses;
-        document.getElementById('balance').innerText = totalBalance;
-        // calculation(' ');
+
+        document.getElementById('balance-input').innerText = totalBalance;
+    
         // Saving calculation
         document.getElementById('save-btn').addEventListener('click', function(){
             // % saving calculation
-            const SaveTaka = document.getElementById('save-money').value;
+            const SaveTaka = idInput('save');
             const saveMoney=parseFloat(SaveTaka);
             const saving=totalIncome*(saveMoney/100);
             console.log(saving);
